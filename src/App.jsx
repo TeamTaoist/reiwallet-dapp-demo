4556e66d4280de6fe359a6862d9cb181b30b4cb8
wendychaung <7518647+wendychaung@users.noreply.github.com>
2024-03-22 01:40:42 +0800

    add transaction & signMessage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { Button } from 'antd';
+import { Button,Input } from 'antd';
 import {useState} from "react";
 import styled from "styled-components";
 import GlobalStyle from "./utils/GlobalStyle";
-import {formatUnit} from "@ckb-lumos/bi";
+import {formatUnit, parseUnit} from "@ckb-lumos/bi";
 
 const Box = styled.div`
     margin: 40px 100px ;
@@ -14,33 +14,101 @@ const Box = styled.div`
             align-content: center;
             gap: 20px;
             border-bottom: 1px solid #ddd;
-            padding-bottom: 10px;
+            padding: 10px 0;
             line-height: 40px;
+            &.noFlex{
+                flex-direction: column;
+             
+            }
         }
+    }
+    .flex{
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        span{
+            flex-shrink: 0;
+            min-width: 100px;
+            background: #f2f2f2;
+            text-align: center;
+        }
+        
+    }
+    .sig{
+        width: 60%;
+        word-break: break-all;
+        line-height: 1em;
     }
 `
 function App() {
 
     const [address,setAddress] = useState('')
-    const [balance,setBalance] = useState("-")
+    const [balance,setBalance] = useState("-");
+    const [message,setMessage] = useState("");
+    const [signature,setSignature] =useState('');
+    const [sendTo,setSendTo] =useState('ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqf5kmkgn25z8xajscwkw88ew3hagjfd5uqttnscm');
+    const [amount,setAmount] =useState('100');
+    const [txHash,setTxhash] = useState('')
+
     const connect = async() =>{
         try{
             let addr =  await window.ckb.request({method:"ckb_requestAccounts"})
             setAddress(addr)
         }catch (e) {
-            console.error("==connect,error=",e)
+            console.error("==connect=",e)
         }
 
     }
     const getBalance = async () =>{
-        console.log(address)
         try{
 
-            let rt =  await window.ckb.request({method:"ckb_getBalance",data:[address]})
+            let rt =  await window.ckb.request({method:"ckb_getCapacity",data:[address]})
             let formatRt = formatUnit(rt,"ckb")
             setBalance(formatRt)
         }catch (e) {
-            console.error("==connect,error=",e)
+            console.error("==getBalance=",e)
+        }
+    }
+
+    const testSign = async() =>{
+        try{
+            let rt = await window.ckb.request({method:"ckb_signMessage",data:{message}})
+            console.log("======",rt)
+            setSignature(rt);
+        }catch (e) {
+            console.error("==signMessage=",e)
+        }
+
+    }
+
+    const handleSend = async() =>{
+
+
+        try{
+            let rt = await window.ckb.request({method:"ckb_sendTransaction",data:{
+                    amount,
+                    to:sendTo
+                }})
+
+            setTxhash(rt);
+        }catch (e) {
+            console.error("==ckb_sendCKB=",e)
+        }
+    }
+
+    const handleInput = (e) =>{
+        const {value,name} = e.target;
+        switch (name){
+            case "message":
+                setMessage(value)
+                break;
+            case "sendTo":
+                setSendTo(value)
+                break;
+            case "amount":
+                setAmount(value)
+                break;
+
         }
     }
 
@@ -64,8 +132,32 @@ function App() {
                 </li>
 
             }
+            {
+                !!address &&<li>
+                <div>
+                    <Input type="text" name="message" value={message} onChange={(e)=>handleInput(e)}/>
+                </div>
+                <div>
+                    <Button type="primary" onClick={() => testSign()}>sign message</Button>
+                </div>
+                <div className="sig">{signature}</div>
+            </li>
+            }
+            {
+                !!address && <li className="noFlex">
+                <div>
+                    <div className="flex"><span>Transfer To</span><Input value={sendTo} name="sendTo" onChange={(e)=>handleInput(e)} /></div>
 
-
+                </div>
+                <div>
+                    <div className="flex"><span>Amount</span><Input value={amount} name="amount" onChange={(e)=>handleInput(e)} /></div>
+                </div>
+                <div>{txHash}</div>
+                <div>
+                    <Button type="primary" onClick={() => handleSend()}>Transaction</Button>
+                </div>
+            </li>
+            }
         </ul>
 
         <GlobalStyle/>
